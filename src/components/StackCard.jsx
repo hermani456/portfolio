7cbbefb87f60a97ccc933ca19cbc06b5b17b99b4
hermani456 <hermani457@gmail.com>
2024-09-22 15:31:59 -0300

    Refactor global CSS to use variables for neon colors and add bottom shadow effect

--- src/components/StackCard.jsx
+++ src/components/StackCard.jsx
@@ -1,7 +1,6 @@
-
-const StackCard = ({name, Icon}) => {
+const StackCard = ({ name, Icon }) => {
   return (
-    <div className="bg-acc/25 w-[8rem] h-[6rem] lg:w-[10rem] lg:h-[8rem] border border-pri rounded-xl py-4 px-3 flex flex-col items-center justify-center mx-5">
+    <div className="bg-acc/25 w-[8rem] h-[6rem] lg:w-[10rem] lg:h-[8rem] border border-pri rounded-xl py-4 px-3 flex flex-col items-center justify-center mx-5 bottom-shadow cursor-pointer">
       <Icon className="fill-pri size-10" />
       <p className="text-white font-exo mt-2">{name}</p>
     </div>
