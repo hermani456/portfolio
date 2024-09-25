7799601eb8b3c92f7ddba6358661ef2a358c62de
hermani456 <hermani457@gmail.com>
2024-09-25 12:19:43 -0300

    Refactor Hero component to extract Terminal component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,13 @@
 "use client";
 import Container from "./Container";
 import Image from "next/image";
-import img from "@/app/img/ardroid.png";
 import doggy from "@/app/img/doggy.webp";
-import doggo from "@/app/img/doggo.webp";
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { socialLinks } from "@/utils";
 import SocialIcon from "./SocialIcon";
-import { SiManjaro } from "react-icons/si";
-import { FaFolderOpen, FaGithubAlt } from "react-icons/fa";
-import { FaCodeBranch } from "react-icons/fa6";
+import Terminal from "./Terminal";
 
 const Hero = () => {
   gsap.registerPlugin(useGSAP);
@@ -50,26 +46,9 @@ const Hero = () => {
         <div></div>
       </div>
       <Container>
-        <div className="flex flex-col lg:flex-row justify-around items-center min-h-[calc(100dvh-5rem)] relative mt-10">
+        <div className="flex flex-col lg:flex-row justify-around items-center min-h-[calc(100dvh-5rem)] relative ">
           <div>
-            <div className="lg:flex font-exo hidden">
-              <div className=" flex h-7">
-                <div className="text-[#1f5668] bg-white w-fit px-5 flex justify-center items-center">
-                  <SiManjaro size={11} />
-                </div>
-                <div className="text-white bg-[#1f5668] px-[1.5rem] flex justify-center items-center path-middle">
-                  <FaFolderOpen size={13} />
-                  <p className="ml-2">~/Projects/portfolio</p>
-                </div>
-                <div className="text-white bg-[#612248] px-[1.5rem] flex justify-center items-center path gap-2">
-                  <FaGithubAlt size={13} />
-                  <FaCodeBranch size={13} />
-                  <p>master</p>
-                </div>
-                {/* blinking cursor */}
-                <div className="cursor ml-4"></div>
-              </div>
-            </div>
+            <Terminal />
             <h1 className="text-white text-4xl lg:text-6xl font-orbitron font-semibold mt-5">
               <span className="neon-text-pink flicker" ref={pinkTextRef}>
                 Hi, I&apos;m
---
+++ src/components/Terminal.jsx
@@ -0,0 +1,27 @@
+import { FaFolderOpen, FaGithubAlt } from "react-icons/fa";
+import { FaCodeBranch } from "react-icons/fa6";
+import { SiManjaro } from "react-icons/si";
+
+const Terminal = () => {
+  return (
+    <div className="lg:flex font-exo hidden">
+      <div className=" flex h-7">
+        <div className="text-[#1f5668] bg-white w-fit px-5 flex justify-center items-center">
+          <SiManjaro size={11} />
+        </div>
+        <div className="text-white bg-[#1f5668] px-[1.5rem] flex justify-center items-center path-middle">
+          <FaFolderOpen size={13} />
+          <p className="ml-2">~/Projects/portfolio</p>
+        </div>
+        <div className="text-white bg-[#612248] px-[1.5rem] flex justify-center items-center path gap-2">
+          <FaGithubAlt size={13} />
+          <FaCodeBranch size={13} />
+          <p>master</p>
+        </div>
+        <div className="cursor ml-4"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Terminal;
