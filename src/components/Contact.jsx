8ed6c1441f0d44930fdbd81f4100fb52211679d2
hermani456 <hermani457@gmail.com>
2024-09-26 19:21:32 -0300

    Add @emailjs/browser dependency to package.json and pnpm-lock.yaml

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,13 +4,14 @@ import Container from "./Container";
 import { FaSpinner } from "react-icons/fa";
 import { GiSpinalCoil } from "react-icons/gi";
 import { GiSpinningBlades } from "react-icons/gi";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
   const form = useRef(null);
   const [name, setName] = useState("");
   const [subject, setSubject] = useState("");
   const [message, setMessage] = useState("");
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   // const notify = () =>
   //   toast.success("Mensaje enviado", {
@@ -24,42 +25,46 @@ const Contact = () => {
   //     theme: "light",
   //   });
 
-  // const sendEmail = (e) => {
-  //   e.preventDefault();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setLoading(true);
 
-  //   emailjs
-  //     .sendForm(
-  //       process.env.NEXT_PUBLIC_MAIL_SERVICE,
-  //       process.env.NEXT_PUBLIC_MAIL_TEMPLATE,
-  //       form.current,
-  //       process.env.NEXT_PUBLIC_MAIL_USER
-  //     )
-  //     .then(
-  //       (result) => {
-  //         console.log(result.text);
-  //       },
-  //       (error) => {
-  //         console.log(error.text);
-  //       },
-  //       e.target.reset(),
-  //       notify()
-  //     );
-  // };
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_MAIL_SERVICE,
+        process.env.NEXT_PUBLIC_MAIL_TEMPLATE,
+        form.current,
+        process.env.NEXT_PUBLIC_MAIL_USER
+      )
+      .then(
+        (result) => {
+          setLoading(false);
+          e.target.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
 
   return (
     <Container id="contact">
       <section className=" p-5" id="contact">
         <div className="max-w-screen-xl mx-auto my-auto text-center">
-        <h2 className="text-white text-3xl lg:text-5xl font-orbitron font-semibold neon-text-blue">
+          <h2 className="text-white text-3xl lg:text-5xl font-orbitron font-semibold neon-text-blue">
             Contact Me
           </h2>
         </div>
-        <form ref={form} className="mx-auto pt-5 max-w-xl sm:pt-14">
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className="mx-auto pt-5 max-w-xl sm:pt-14"
+        >
           <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
             <div className="sm:col-span-2">
               <label
                 htmlFor="name"
-                className="block text-sm font-semibold leading-6 text-sec font-orbitron" 
+                className="block text-sm font-semibold leading-6 text-sec font-orbitron"
               >
                 Name
               </label>
@@ -68,10 +73,10 @@ const Contact = () => {
                   required
                   type="text"
                   name="name"
-                  value={name}
+                  // value={name}
                   id="name"
                   className="block w-full rounded-md border-0 px-3.5 py-2 bg-back text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
-                  onChange={(e) => setName(e.target.value)}
+                  // onChange={(e) => setName(e.target.value)}
                 />
               </div>
             </div>
@@ -87,10 +92,10 @@ const Contact = () => {
                   required
                   type="text"
                   name="subject"
-                  value={subject}
+                  // value={subject}
                   id="subject"
                   className="block w-full rounded-md border-0 px-3.5 py-2 bg-back text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
-                  onChange={(e) => setSubject(e.target.value)}
+                  // onChange={(e) => setSubject(e.target.value)}
                 />
               </div>
             </div>
@@ -106,10 +111,10 @@ const Contact = () => {
                   required
                   type="text"
                   name="message"
-                  value={message}
+                  // value={message}
                   id="message"
                   className="block w-full rounded-md border-0 px-3.5 py-2 min-h-[6rem] max-h-[10rem] bg-back text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
-                  onChange={(e) => setMessage(e.target.value)}
+                  // onChange={(e) => setMessage(e.target.value)}
                 />
               </div>
             </div>
