3aaffac54cc0230005e25c17c6aec3e61f879b73
diego <hermani457@gmail.com>
2025-06-14 01:22:17 -0400

    Add Liquid App project details and image to projects list

--- src/utils/index.js
+++ src/utils/index.js
@@ -14,36 +14,37 @@ import { IoLogoJavascript } from "react-icons/io5";
 import { TbBrandReactNative } from "react-icons/tb";
 import { IoMdHome } from "react-icons/io";
 import { FaProjectDiagram } from "react-icons/fa";
-import { FaInfoCircle } from "react-icons/fa"
-import { FaPhoneAlt } from "react-icons/fa"
+import { FaInfoCircle } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
 import wedding from "@/app/img/wedding.webp";
 import wilko from "@/app/img/wilko.webp";
 import yme from "@/app/img/yme.webp";
+import liquidapp from "@/app/img/liquidapp.png";
 
 export const navLinks = [
   {
     id: 1,
     name: "Home",
     path: "#home",
-    icon: IoMdHome
+    icon: IoMdHome,
   },
   {
     id: 2,
     name: "About",
     path: "#about",
-    icon: FaInfoCircle
+    icon: FaInfoCircle,
   },
   {
     id: 3,
     name: "Projects",
     path: "#projects",
-    icon: FaProjectDiagram
+    icon: FaProjectDiagram,
   },
   {
     id: 4,
     name: "Contact",
     path: "#contact",
-    icon: FaPhoneAlt
+    icon: FaPhoneAlt,
   },
 ];
 
@@ -132,29 +133,57 @@ export const projects = [
   {
     id: 1,
     name: "Wedding RSVP",
-    description: "A wedding RSVP app that allows guests to respond to wedding invitations. Upon successful RSVP, guests receive both an email and a PDF invitation.",
-    techStack: ["NextJS", "TailwindCSS", "PostgreSQL", "ReactJS", "Resend", "React Email", "React PDF"],
+    description:
+      "A wedding RSVP app that allows guests to respond to wedding invitations. Upon successful RSVP, guests receive both an email and a PDF invitation.",
+    techStack: [
+      "NextJS",
+      "TailwindCSS",
+      "PostgreSQL",
+      "ReactJS",
+      "Resend",
+      "React Email",
+      "React PDF",
+    ],
     github: "https://github.com/hermani456/wedding",
     live: "https://weddingrsvp.diegocampuzano.me/",
     img: wedding,
   },
   {
     id: 2,
+    name: "Liquid App",
+    description:
+      "A web app that allows users to create and manage payrolls, including the ability to create companies, employees, and payrolls. It also includes a feature to send payslips via email.",
+    techStack: [
+      "NextJS",
+      "TailwindCSS",
+      "Shadcn UI",
+      "PostgreSQL",
+      "ReactJS",
+      "Resend",
+      "React Email",
+    ],
+    github: "https://github.com/hermani456/liquidapp",
+    live: "https://liquidapp.diegocampuzano.me/",
+    img: liquidapp,
+  },
+  {
+    id: 3,
     name: "A Company Services Web Page",
-    description: "A web page highlighting the diverse range of services offered by the company.",
+    description:
+      "A web page highlighting the diverse range of services offered by the company.",
     techStack: ["NextJS", "TailwindCSS", "ReactJS", "EmailJS", "Frame Motion"],
     github: "https://github.com/hermani456/willka-next",
     live: "https://willko.diegocampuzano.me",
     img: wilko,
   },
   {
-    id: 3,
+    id: 4,
     name: "Web Page for a Recruitment Agency",
-    description: "A web page for a recruitment agency that enables users to upload their CVs easily.",
+    description:
+      "A web page for a recruitment agency that enables users to upload their CVs easily.",
     techStack: ["NextJS", "TailwindCSS", "ReactJS", "Resend", "GSAP"],
     github: "https://github.com/hermani456/yme",
     live: "https://yme.diegocampuzano.me/",
     img: yme,
   },
-
-]
+];
